9a71311e1ecb852d286a4900068d7622b3e78c6e
whitgunt77 <whitgunt77@gmail.com>
2025-11-24 11:15:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 
 export default function App() {
