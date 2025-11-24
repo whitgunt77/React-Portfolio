216a234de035be4e8dd6134ef0ab6b801bd67544
whitgunt77 <whitgunt77@gmail.com>
2025-11-24 11:21:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import { MoonIcon, SunIcon } from './icons'
 
 
