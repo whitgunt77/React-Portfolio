2a225bc0460d6f785b4a3b875791c25adebda63e
whitgunt77 <whitgunt77@gmail.com>
2025-11-24 10:05:11 -0500

    Create icons.jsx

---
+++ src/icons.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export const MoonIcon = (props) => (
+<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+)
+
+
+export const SunIcon = (props) => (
+<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="1.5" />
+<path d="M12 3v1M12 20v1M4.2 4.2l.7.7M19.1 19.1l.7.7M1 12h1M22 12h1M4.2 19.8l.7-.7M19.1 4.9l.7-.7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+)
